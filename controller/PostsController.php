cffc56a2dea65262e8082d2a242a1411f469da75
DenitsaDremsizova <denitsa.dremsizova@gmail.com>
2017-04-24 11:44:19 +0300

    code refactoring

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -13,122 +13,92 @@ if (isset($_SESSION['userId']) && isset($_SESSION['timelineId'])) {
     $dao = new PostDAO();
 
     if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
+
+        //validate if user is allowed to post on timeline:
         if ($userId !== $timelineId) {
             $daoFriend = new FriendDAO;
             if (!($daoFriend->checkIfInFriendsList($userId, $timelineId))) {
                 throw new Exception('You are not alllowed to post on the timeline of people outside your friends list.');
             }
         }
-        
+
         //delete prvious error message:
         unset($_SESSION['error-msg']);
         
-        //check for uploaded picture
+        function uploadFile($fileType) {
+        
+        if ($fileType === 'photo') {
+            $uploadedFileName = $_FILES['uploaded-photo']['tmp_name'];
+            $fileOriginalName = $_FILES['uploaded-photo']['name'];
+            $requiredMIMEtype = 'image';
+            $authorId = $_POST['uploaded-photo-authorId'];
+            $timelineId = $_POST['uploaded-photo-timelineId'];
+            $type = 'photos';
+            $text = htmlentities(trim($_POST['uploaded-photo-text']));
+        } elseif ($fileType === 'video') {
+            $uploadedFileName = $_FILES['uploaded-video']['tmp_name'];
+            $fileOriginalName = $_FILES['uploaded-video']['name'];
+            $requiredMIMEtype = 'video';
+            $authorId = $_POST['uploaded-video-authorId'];
+            $timelineId = $_POST['uploaded-video-timelineId'];
+            $type = 'uploaded_videos';
+            $text = htmlentities(trim($_POST['uploaded-video-text']));
+        }
+        
+        try {
+            if ($uploadedFileName !== "" && $uploadedFileName !== null) {
+
+                //get file extension:
+                $fileOriginalName = explode(".", $fileOriginalName);
+                $extension = end($fileOriginalName);
+                
+                //vallidate MIME type:
+                $mimeType = explode("/", mime_content_type($uploadedFileName))[0];
+                
+                if ($mimeType === $requiredMIMEtype) {
+                    
+                    //send data to PostDAO.php to add post to DB:
+
+                    $newPost = new Post($authorId, $timelineId, $type, $text);                    
+                    $newPost->extension = $extension;
+                    $newDAO = new PostDAO();
+                    $newDAO->addPost($newPost);
+                    
+                    //upload photo to server:
+
+                    if (isset($_SESSION['fileName'])) {
+                        if (is_uploaded_file($uploadedFileName)) {
+                            if (move_uploaded_file($uploadedFileName, $_SESSION['fileName'])) {
+                                
+                            } else {
+                                //TO DO: delete post from DB
+                                throw new Exception('Moving file failed');
+                            }
+                        } else {
+                            //TO DO: delete post from DB
+                            throw new Exception('Uploading file failed.');
+                        }
+                    }
+                } else {
+                    throw new Exception('Invalid file type!');
+                }
+            } else {
+                throw new Exception('No file attached!');
+            }
+        } catch (Exception $e) {
+            $_SESSION['error-msg'] = $e->getMessage();
+        } finally {
+            //go to timeline:
+            header('Location:./TimelineController.php?timelineId=' . $timelineId, true, 302);
+        }
+    }
+
+        //upload file:    
         if (isset($_POST['upload-photo'])) {
-            try {
-                if (isset($_FILES['uploaded-photo'])) {
-
-                    //get file extension:
-                    $uploadedPicName = $_FILES['uploaded-photo']['tmp_name'];
-                    $picOriginalName = $_FILES['uploaded-photo']['name'];
-                    $picOriginalName = explode(".", $picOriginalName);
-                    $extension = end($picOriginalName);
-
-                    //vallidate MIME type:
-                    $mimeType = explode("/", mime_content_type($uploadedPicName))[0];
-
-                    if ($mimeType === "image") {
-
-                        //send data to PostDAO.php to add post to DB:
-                        $authorId = $_POST['uploaded-photo-authorId'];
-                        $timelineId = $_POST['uploaded-photo-timelineId'];
-                        $type = "photos";
-                        $text = htmlentities(trim($_POST['uploaded-photo-text']));
-                        $newPost = new Post($authorId, $timelineId, $type, $text);
-                        $newPost->extension = $extension;
-                        $dao->addPost($newPost);
-
-                        //upload photo to server:
-
-                        if (isset($_SESSION['fileName'])) {
-                            if (is_uploaded_file($uploadedPicName)) {
-                                if (move_uploaded_file($uploadedPicName, $_SESSION['fileName'])) {
-                                    
-                                } else {
-                                    //TO DO: delete post from DB
-                                    throw new Exception('Moving file failed');
-                                }
-                            } else {
-                                //TO DO: delete post from DB
-                                throw new Exception('Uploading file failed.');
-                            }
-                        }
-                    } else {
-                        throw new Exception('Invalid file type!');
-                    }
-                } else {
-                    throw new Exception('No file attached!');
-                }
-            } catch (Exception $e) {
-                $_SESSION['error-msg'] = $e->getMessage();
-            } finally {
-                //go to timeline:
-                header('Location:./TimelineController.php?timelineId=' . $timelineId, true, 302);
-            }
+            uploadFile('photo');
         } elseif (isset($_POST['upload-video'])) {
-            try {
-                if (isset($_FILES['uploaded-video'])) {
-
-                    //get file extension:
-                    $uploadedFileName = $_FILES['uploaded-video']['tmp_name'];
-                    $fileOriginalName = $_FILES['uploaded-video']['name'];
-                    $fileOriginalName = explode(".", $fileOriginalName);
-                    $extension = end($fileOriginalName);
-
-                    //vallidate MIME type:
-
-                    $mimeType = explode("/", mime_content_type($uploadedFileName))[0];
-
-                    if ($mimeType === "video") {
-
-                        //send data to PostDAO.php to add post to DB:
-                        $authorId = $_POST['uploaded-video-authorId'];
-                        $timelineId = $_POST['uploaded-video-timelineId'];
-                        $type = "uploaded_videos";
-                        $text = htmlentities(trim($_POST['uploaded-video-text']));
-                        $newPost = new Post($authorId, $timelineId, $type, $text);
-                        $newPost->extension = $extension;
-                        $dao->addPost($newPost);
-
-                        //upload video to server:
-
-                        if (isset($_SESSION['fileName'])) {
-                            if (is_uploaded_file($uploadedFileName)) {
-                                if (move_uploaded_file($uploadedFileName, $_SESSION['fileName'])) {
-                                    
-                                } else {
-                                    //TO DO: delete post from DB
-                                    throw new Exception('Moving file failed');
-                                }
-                            } else {
-                                //TO DO: delete post from DB
-                                throw new Exception('Uploading file failed.');
-                            }
-                        }
-                    } else {
-                        throw new Exception('Invalid file type!');
-                    }
-                } else {
-                    throw new Exception('No file attached!');
-                }
-            } catch (Exception $e) {
-                $_SESSION['error-msg'] = $e->getMessage();
-            } finally {
-                //go to timeline:
-                header('Location:./TimelineController.php?timelineId=' . $timelineId, true, 302);
-            }
-            
-    } else {
+            uploadFile('video');
+        } else {
             // add new text post
             $textPost = json_decode($_POST['data']);
 
