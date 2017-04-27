201e23f7639e411f5c2478b7e42683ae91816e0d
DenitsaDremsizova <denitsa.dremsizova@gmail.com>
2017-04-27 10:09:38 +0300

    logo changed

--- view/header.php
+++ view/header.php
@@ -37,7 +37,7 @@ if(!isset($controller)) {
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="TimelineController.php"><img src="../view/images/logo.png" alt="logo" /></a>
+            <a class="navbar-brand" href="TimelineController.php"><img src="../view/images/new-logo-white.png" alt="logo" style="width: 168px; height:30px"/></a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
